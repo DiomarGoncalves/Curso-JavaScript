a9ea272314f4d221cb67739b7f4f4c26b5c903b1
Diomar Gonçalves <50972479+DiomarBR@users.noreply.github.com>
2023-05-23 14:56:10 +0000

    update DOM

---
+++ Módulo B/ex006.js
@@ -0,0 +1,9 @@
+var numero1 = 5
+var numero2 = 2
+
+var mult = numero1 * numero2
+var som = numero1 + numero2
+var sub = numero1 - numero2
+var divi = numero1 / numero2
+
+window.alert(`a multiplicação e : ${mult}, a soma e : ${som}, a subitação e : ${sub}, a divisão e : ${divi}`)
