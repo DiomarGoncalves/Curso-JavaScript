ce7fb9eb52654d04c7a91ca3c350887999e8af4c
Diomar Gonçalves <50972479+DiomarBR@users.noreply.github.com>
2023-05-23 21:01:09 -0300

    update modulo D

---
+++ modulo D/aula01.js
@@ -0,0 +1,24 @@
+// ex 008
+function calcular(){
+    var velocidade = document.getElementById('vel');
+    const res = document.getElementById('res');
+    var vel = Number(velocidade.value)
+
+    res.innerHTML = `Sua velocidade atual e de <stron>${vel}km/h</stron>`
+    if(vel > 60){
+        res.innerHTML += `<p>Você está MUTADO!</p>`
+    }
+    res.innerHTML += `<p>Diriga sempre com segurança</p>`
+
+}
+//ex009
+function nacio(){
+    const local = document.getElementById('pais');
+    const resp = document.getElementById('resp');
+    loc = String(local.value)
+    if (loc == 'Brasil'){
+        resp.innerHTML = `Brasileiro`
+    }else{
+        resp.innerHTML = `estrangeiro`
+    }
+}
