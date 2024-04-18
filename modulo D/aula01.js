9d1a2cda5e6231ac0357ca6729cd5887264c4ea9
Diomar Gonçalves <50972479+DiomarBR@users.noreply.github.com>
2024-04-18 14:29:45 -0300

    Update aula01.js

--- modulo D/aula01.js
+++ modulo D/aula01.js
@@ -4,7 +4,7 @@ function calcular(){
     const res = document.getElementById('res');
     var vel = Number(velocidade.value)
 
-    res.innerHTML = `Sua velocidade atual e de <stron>${vel}km/h</stron>`
+    res.innerHTML = `Sua velocidade atual e de <strong>${vel}km/h</strong>`
     if(vel > 60){
         res.innerHTML += `<p>Você está MUTADO!</p>`
     }
